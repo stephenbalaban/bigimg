391d4c845e35adf716ac055c7e4777b69e8f9901
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-02-15 02:59:01 -0800

    chmod -x on bigimg and added __init__

---
+++ bigimg/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""Script for generating large images."""
+
+__author__ = ['stephen balaban <stephen@stephenbalaban.com>']
+__version__ = "0.0.2"
+__license__ = "public domain"
+__contributors__ = "see AUTHORS"
+
+from bigimg import lossless_random, main
--- bigimg/bigimg.py
+++ bigimg/bigimg.py
@@ -14,36 +14,39 @@
     Copying and distribution of this file, with or without modification,
     are permitted in any medium without royalty provided the copyright
     notice and this notice are preserved.
-
 """
+
 import sys
-import numpy as np
-numpy = np
+import numpy
 import scipy.misc
 import math
 import random
 import string
 
-
 def cleanfn(fname):
-    valid = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    valid = string.ascii_lowercase + string.ascii_uppercase + \
+        string.digits
     return ''.join(c for c in fname if c in valid)
 
 
-def make_image(width, height, function, outfilepng):
-    imat = np.fromfunction(function, (width, height), dtype=float)
-    print(imat)
-    img = scipy.misc.toimage(imat, high=np.max(imat), low=np.min(imat))
+def make_image(width, height, function, outfilepng, verbose=False):
+    imat = numpy.fromfunction(function, (width, height), dtype=float)
+    if verbose:
+        print(imat)
+    img = scipy.misc.toimage(imat, high=numpy.max(imat),
+                             low=numpy.min(imat))
     img.save(outfilepng)
     return imat
 
 
-def main(w, h, fs, o, gen_name=False):
+def main(w, h, fs, o, gen_name=False, verbose=False):
     f = eval(fs)
     w = int(w)
     h = int(h)
-    print("# Making matrix image of size (%s, %s) with %s mapped elementwise."
-            % (w, h, fs))
+
+    if verbose:
+        print("# Making matrix image of size (%s, %s) with " \
+                  "%s mapped elementwise." % (w, h, fs))
     # generate a name using a hash of the function
     if gen_name:
         o = "%s_out.png" % cleanfn(fs)
@@ -51,22 +54,19 @@ def main(w, h, fs, o, gen_name=False):
 
 
 def lossless_random(w, h, fs, o):
+    """
+    params:
+        w - width
+        h - height
+        fs - function
+        o - outfile
+    """
     w = int(w)
     h = int(h)
-    a = np.random.uniform(0, 2**16 - 1, (w, h)).astype('int32')
-    img = scipy.misc.toimage(a, high=np.max(a), low=np.min(a), mode='I')
+    a = numpy.random.uniform(0, 2**16 - 1, (w, h)).astype('int32')
+    img = scipy.misc.toimage(a, high=numpy.max(a),
+                             low=numpy.min(a), mode='I')
     img.save(o)
 
 
-if __name__ == '__main__':
-    w, h, fs, o = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-    if fs == 'random':
-        lossless_random(w,h,fs,o)
-    else:
-        main(w, h, fs, o)
-        try:
-            pass
-        except Exception as e:
-            print("Exception: %s" % e)
-            print("Usage: ./bigimg.py w h f(dyadic lambda expression)"
-                + " outfile.png")
+
